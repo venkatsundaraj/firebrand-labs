29994e6282d9efbd4a569e30d8ad7fca36dbcb40
venkatsundaraj <venkateshsundarasan@gmail.com>
2024-07-31 18:10:30 +0530

    vercel-commit

---
+++ src/components/carousel-branding.tsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
+export function CarouselDemo() {
+  return (
+    <Carousel className="w-full max-w-xs">
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1"></div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+}
